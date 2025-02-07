99b85690b6e4daa5cf07977b7f22735a0e3b8385
mrnoob0dy <jayna5155@yandex.ru>
2025-02-07 16:13:02 +0300

    Added lodash

--- webapp/src/lib/routes.ts
+++ webapp/src/lib/routes.ts
@@ -1,2 +1,14 @@
+const getRouteParams = <T extends Record<string, boolean>>(object: T) => {
+    return Object.keys(object).reduce((acc, key) => ({...acc, [key]: `:${key}`}), {}) as Record<keyof T, string>
+}
+
 export const getAllIdeasRoute = () => '/'
-export const getViewIdeaRoute = ({ideaNick}: {ideaNick: string}) => `/ideas/${ideaNick}`
+
+
+export const viewIdeaRouteParams = getRouteParams({ideaNick: true})
+export type ViewIdeaRouteParams = typeof viewIdeaRouteParams
+export const getViewIdeaRoute = ({ideaNick}: ViewIdeaRouteParams) => `/ideas/${ideaNick}`
+
+// export const viewIdeaRouteParams = {ideaNick: ':ideaNick'}
+// export type viewIdeaRouteParams = {ideaNick: string}
+// export const getViewIdeaRoute = ({ideaNick}: {ideaNick: string}) => `/ideas/${ideaNick}`
