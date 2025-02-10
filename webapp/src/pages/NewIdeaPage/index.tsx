94b3efc83ff0c6b9b71501a3e20184a6403a473b
mrnoob0dy <jayna5155@yandex.ru>
2025-02-10 12:14:40 +0300

    Created form(new idead)

--- webapp/src/pages/NewIdeaPage/index.tsx
+++ webapp/src/pages/NewIdeaPage/index.tsx
@@ -1,10 +1,33 @@
-import css from './index.module.scss';
+import { useState } from 'react'
+import { Segment } from '../../components/Segment'
+import css from './index.module.scss'
+import { Input } from '../../components/Input/Index'
+import { Textarea } from '../../components/Textarea'
 
 export const NewIdeaPage = () => {
-    return (
-        <div>
-            <h1 className={css.title}>New Idea</h1>
-            <div>From will be here...</div>
-        </div>
-    )
+  const [state, setState] = useState({
+    name: '',
+    nick: '',
+    description: '',
+    text: '',
+  })
+
+  return (
+    <Segment title="New Idea">
+      <form
+        className={css.form}
+        onSubmit={(e) => {
+          e.preventDefault()
+          console.info('Submitted', state)
+        }}
+      >
+        <Input name='name' label='Name' state={state} setState={setState} />
+        <Input name='nick' label='Nick' state={state} setState={setState} />
+        <Input name='description' label='Description' state={state} setState={setState} />
+        <Textarea name='text' label='Text' state={state} setState={setState} />
+
+        <button type="submit">Create Idea</button>
+      </form>
+    </Segment>
+  )
 }
