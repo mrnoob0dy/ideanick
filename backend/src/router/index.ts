6009dd60763cb0503d80aafe8587489c6e008c14
mrnoob0dy <jayna5155@yandex.ru>
2025-02-15 09:43:17 +0300

    Добавление tRPC мутации

--- backend/src/router/index.ts
+++ backend/src/router/index.ts
@@ -1,11 +1,13 @@
 import { trpc } from "../lib/trpc";
+import { createIdeaTrpcRoute } from "./createIdea";
 import { getIdeaTrpcRoute } from "./getIdea";
 import { getIdeasTrpcRoute } from "./getIdeas";
 
 
 export const trpcRouter = trpc.router({
     getIdea: getIdeaTrpcRoute,
-    getIdeas: getIdeasTrpcRoute
+    getIdeas: getIdeasTrpcRoute,
+    createIdea: createIdeaTrpcRoute
 });
 
 export type TrpcRouter = typeof trpcRouter;
--- webapp/src/pages/NewIdeaPage/index.tsx
+++ webapp/src/pages/NewIdeaPage/index.tsx
@@ -5,8 +5,10 @@ import { Input } from '../../components/Input/Index'
 import { Textarea } from '../../components/Textarea'
 import { withZodSchema } from 'formik-validator-zod'
 import { z } from 'zod'
+import { trpc } from '../../lib/trpc'
 
 export const NewIdeaPage = () => {
+    const createIdea = trpc.createIdea.useMutation()
     const formik = useFormik({
         initialValues: {
             name: '',
@@ -25,8 +27,8 @@ export const NewIdeaPage = () => {
               text: z.string().min(100, 'Text should be at least 100 characters long'),
             })
           ),
-        onSubmit: (values) => {
-            console.log(values)
+        onSubmit: async (values) => {
+            await createIdea.mutateAsync(values)
         }
     })
 
