d9d6d4d8d4143235a231645dfa0e6c853efb542b
mrnoob0dy <jayna5155@yandex.ru>
2025-02-25 21:15:18 +0300

    Передача клиента Prisma в tRPC контекст

--- backend/src/lib/trpc.ts
+++ backend/src/lib/trpc.ts
@@ -2,13 +2,15 @@ import { initTRPC } from "@trpc/server";
 import * as trpcExpress from '@trpc/server/adapters/express';
 import { type Express } from "express";
 import { TrpcRouter } from "../router";
+import { AppContext } from "./ctx";
 
-export const trpc = initTRPC.create();
-export const applyTrpcToExpressApp = (expressApp: Express, trpcRouter: TrpcRouter) => {
+export const trpc = initTRPC.context<AppContext>().create();
+export const applyTrpcToExpressApp = (expressApp: Express, appContext: AppContext, trpcRouter: TrpcRouter) => {
     expressApp.use(
         "/trpc",
         trpcExpress.createExpressMiddleware({
             router: trpcRouter,
+            createContext: () => appContext,
         })
     );
 }
--- backend/src/router/getIdeas/index.ts
+++ backend/src/router/getIdeas/index.ts
@@ -1,7 +1,15 @@
-import { ideas } from "../../lib/ideas"
 import { trpc } from "../../lib/trpc"
 import _ from "lodash";
 
-export const getIdeasTrpcRoute = trpc.procedure.query(() => {
-    return { ideas: ideas.map((idea) => _.pick(idea, ['nick', 'name', 'description'])) };
+export const getIdeasTrpcRoute = trpc.procedure.query( async ({ctx}) => {
+    const ideas = await ctx.prisma.idea.findMany({
+        select: {
+            id: true,
+            nick: true,
+            name: true,
+            description: true
+        }
+    })
+
+    return { ideas }
 })
