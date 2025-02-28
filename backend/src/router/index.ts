91ac1127687fabd600304a97ead3460a13a8547d
mrnoob0dy <jayna5155@yandex.ru>
2025-02-28 21:33:24 +0300

    Авторизация: модель пользователя + эндпоинт регистрации

--- backend/src/router/index.ts
+++ backend/src/router/index.ts
@@ -2,12 +2,14 @@ import { trpc } from "../lib/trpc";
 import { createIdeaTrpcRoute } from "./createIdea";
 import { getIdeaTrpcRoute } from "./getIdea";
 import { getIdeasTrpcRoute } from "./getIdeas";
+import { signUpTrpcRoute } from "./signUp";
 
 
 export const trpcRouter = trpc.router({
     getIdea: getIdeaTrpcRoute,
     getIdeas: getIdeasTrpcRoute,
-    createIdea: createIdeaTrpcRoute
+    createIdea: createIdeaTrpcRoute,
+    signUp: signUpTrpcRoute
 });
 
 export type TrpcRouter = typeof trpcRouter;
