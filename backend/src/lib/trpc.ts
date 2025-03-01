a98cf5e282e6351f42454df8a924360ccd758e63
mrnoob0dy <jayna5155@yandex.ru>
2025-03-01 09:25:15 +0300

    Использование инструмента для ручного вызова tRPC эндпоинтов

--- backend/src/lib/trpc.ts
+++ backend/src/lib/trpc.ts
@@ -3,15 +3,28 @@ import { type Express } from 'express'
 import { type TrpcRouter } from '../router'
 import { type AppContext } from './ctx'
 import { initTRPC } from '@trpc/server'
+import { expressHandler } from 'trpc-playground/handlers/express'
 
 export const trpc = initTRPC.context<AppContext>().create({})
 
-export const applyTrpcToExpressApp = (expressApp: Express, appContext: AppContext, trpcRouter: TrpcRouter) => {
+export const applyTrpcToExpressApp = async (expressApp: Express, appContext: AppContext, trpcRouter: TrpcRouter) => {
   expressApp.use(
     '/trpc',
     trpcExpress.createExpressMiddleware({
       router: trpcRouter,
       createContext: () => appContext,
+    })
+  )
+
+  expressApp.use(
+    '/trpc-playground',
+    await expressHandler({
+      trpcApiEndpoint: '/trpc',
+      playgroundEndpoint: '/trpc-playground',
+      router: trpcRouter,
+      request: {
+        superjson: true,
+      },
     })
   )
 }
