d3f9f6954aebe63f4a420a822ef20460c4814c62
mrnoob0dy <jayna5155@yandex.ru>
2025-03-07 22:04:22 +0300

    Environment Variables: backend

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -4,6 +4,8 @@ import { type AppContext, createAppContext } from './lib/ctx'
 import { applyTrpcToExpressApp } from './lib/trpc'
 import { trpcRouter } from './router'
 import { applyPassportToExpressApp } from './lib/passport'
+import { env } from './lib/env'
+
 
 void (async () => {
   let ctx: AppContext | null = null
@@ -16,8 +18,8 @@ void (async () => {
     })
     applyPassportToExpressApp(expressApp, ctx)
     await applyTrpcToExpressApp(expressApp, ctx, trpcRouter)
-    expressApp.listen(3000, () => {
-      console.info('Listening at http://localhost:3000')
+    expressApp.listen(env.PORT, () => {
+      console.info(`Listening at http://localhost:${env.PORT}`)
     })
   } catch (error) {
     console.error(error)
