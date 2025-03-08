de014909b3772a8ec0a662b1556e9c1359e373e9
mrnoob0dy <jayna5155@yandex.ru>
2025-03-08 18:39:00 +0300

    Environment Variables: webapp

--- webapp/src/lib/trpc.tsx
+++ webapp/src/lib/trpc.tsx
@@ -4,6 +4,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { httpBatchLink } from "@trpc/client";
 import React from "react";
 import Cookies from 'js-cookie'
+import { env } from "./env";
 
 export const trpc = createTRPCReact<TrpcRouter>();
 const queryClient = new QueryClient({
@@ -18,7 +19,7 @@ const queryClient = new QueryClient({
 const trpcClient = trpc.createClient({
     links: [
         httpBatchLink({
-            url: "http://localhost:3000/trpc",
+            url: env.VITE_BACKEND_TRPC_URL,
 
             headers: () => {
                 const token = Cookies.get('token')
